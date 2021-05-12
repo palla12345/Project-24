1da0d8130734726b18b39e6b7d93179cb72399bb
palla12345 <73012299+palla12345@users.noreply.github.com>
2021-05-12 16:56:20 +0530

    Add files via upload

---
+++ Rubber.js
@@ -0,0 +1,34 @@
+class Rubber{
+	constructor(x,y,r)
+	{
+		var options={
+			restitution:0.3,
+			friction:0.8,
+		    density:0.1,
+		}
+	// assign options to the rubber ball
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+       
+	}
+	display()
+	{
+		var pos=this.body.position;	
+			var angle=this.body.angle;
+			push()
+			translate(pos.x, pos.y);
+		    rotate(angle);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("darkblue");
+			//draw the ellipse here to display the rubber ball
+			
+			 ellipse(0,0,this.r,this.r);
+			pop()
+	}
+
+}
---
+++ Stone.js
@@ -0,0 +1,27 @@
+class Stone {
+    constructor(x, y) {
+      var options = {
+        'density':30,
+        'friction': 3,
+        'restitution':0.3
+      };
+      this.body = Bodies.rectangle(x, y, 50, 50, options);
+      this.width = 100;
+      this.height = 40;
+      World.add(world, this.body);
+    };
+    display(){
+        var pos =this.body.position;
+        var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke('white')
+      fill('grey')
+      rectMode(CENTER)
+      rect(0, 0, this.width, this.height);
+      pop();
+    };
+  };
